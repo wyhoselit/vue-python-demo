151d9a42bc010978398f6ec245b8b8276d4e07c5
slowhand <slowhand.fs@gmail.com>
2026-07-27 11:12:10 +0800

    openspec archive

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,16 @@
 (cd /code/vue-python-demo/; podman-compose down && podman-compose up -d --build)
 (cd /code/vue-python-demo/; podman-compose ps )
 
+openspec list
+# openspec archive add-user-authentication-with-jwt-and-cookiesn
+# openspec archive fix-frontend-login-cookie 
+# openspec archive fix-login-register-ref-and-404
+# openspec archive add-users-me-endpoint
+# openspec archive improve-auth-error-handling-and-logging --no-validate
+# openspec archive fix-auto-migration-on-startup
+# openspec list
 
-
+openspec status
 # (cd /code/vue-python-demo/; gitnexus analyze .)
 # (cd /code/vue-python-demo/; gitnexus wiki .)
 # (cd /code/vue-python-demo/; openwiki --update )
